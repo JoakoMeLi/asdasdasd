101122bbe43b95b643fa3579bcd859338124f976
JoakoMeLi <joaquin.vasquez@mercadolibre.com>
2022-09-27 10:29:58 -0300

    Cambio en linea 6 desde branch ramaUno

--- helloworld.go
+++ helloworld.go
@@ -3,7 +3,7 @@ package asdasdasd
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Super Hello World")
 	fmt.Println("Texto desde el branch Main y editado desde el branch ramaUno")
 	fmt.Println("Texto desde el branch ramaUno")
 }
